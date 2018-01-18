d71758f378acf0a56c9f8060453c7962c0baa4ac
Matthew Daniel Murphy <matt@mattmurphy.ca>
2018-01-18 12:29:16 -0500

    ex6: link employees table to stores table; add 3 employees each to store1 & store2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,14 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create first_name: 'Matt', last_name: 'Murphy', hourly_rate: 100
+
+@store1.employees.create first_name: 'Tom', last_name: 'Murphy', hourly_rate: 100
+
+@store1.employees.create first_name: 'Dave', last_name: 'Murphy', hourly_rate: 100
+
+@store2.employees.create first_name: 'Jim', last_name: 'Murphy', hourly_rate: 100
+
+@store2.employees.create first_name: 'Sally', last_name: 'Murphy', hourly_rate: 100
+
+@store2.employees.create first_name: 'Steven', last_name: 'Murphy', hourly_rate: 100
